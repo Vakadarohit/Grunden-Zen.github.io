95bdb55fb8e351ad3d237157b601421003d1dd79
Vakadarohit <93326858+Vakadarohit@users.noreply.github.com>
2022-04-30 11:43:10 +0530

    Update contact handler.php

--- contact handler.php
+++ contact handler.php
@@ -1,22 +1,12 @@
 <?php
-   $name=$_POST['name'];
-   $email=$_POST['email'];
-   $messege=$_POST['messege'];
-    
-   $mail_from='$email';
 
-   $mail_topic="new application submission";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-   $email_body="User Name: $name. \n". "User email: $email. \n". "Messege: $messege. \n";
+$mailheader = "From:".$name."<".$email.">\r\n";
 
-   $to="vakadarohit56@gmail.com";
+$recipient = "vakadarohit56@gmail.com";
 
-   $headers= "From: $mail_from \r\n";
-
-   $headers= "Reply To: $email \r\n";
-
-   mail($to, $mail_topic, $email_body, $headers );
-
-   header("Location: Home.html");
-
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
 ?>
